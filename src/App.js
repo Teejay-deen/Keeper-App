0542be7c09c9196415e2c7c6990ac19efda69abb
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-04 16:59:37 +0100

    adding map function to map through the notes on notes.jsx

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Note from './Components/Note';
+import notes from './Components/Notes';
+
+function NotesCreated(notes){
+  return (
+    <Note key={notes.id} title={notes.title} content={notes.content} />
+  )
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(NotesCreated)}
       <Footer />
     </div>
   );
