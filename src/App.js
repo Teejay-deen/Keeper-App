621302821b1184126e954efbbf1f793c04c5a458
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-04 17:44:56 +0100

    using ES6 arrow function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Note from "./Components/Note";
+import notes from "./Components/Notes";
 
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Note from './Components/Note';
-import notes from './Components/Notes';
-
-function NotesCreated(notes){
-  return (
-    <Note key={notes.id} title={notes.title} content={notes.content} />
-  )
-}
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(NotesCreated)}
+      {notes.map((notes) => 
+   <Note key={notes.id} title={notes.title} content={notes.content} />
+
+)}
       <Footer />
     </div>
   );
